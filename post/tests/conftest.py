09507d722798e4f174e93f672b46f8970c31b81c
Chad DeRosier <chad.derosier@gmail.com>
2020-01-03 18:11:28 -0800

    post models fully tested

--- post/tests/conftest.py
+++ post/tests/conftest.py
@@ -12,12 +12,11 @@ def post_w_path_content(db):
     content = '<p>Hello!!!</p> <p>Hello again.</p>'
 
     mixer.blend(User)
-    category = mixer.blend(PostCategory,
-                           category='Cat 1',
-                           slug_category='cat1')
+    mixer.blend(PostCategory, category='Cat 1', slug_category='cat1')
     post = mixer.blend(Post,
                        content=content,
                        slug_post='heres-a-slug',
                        category_id=1,
                        publish_date=datetime(2018, 8, 15))
+
     yield post, '/cat1/2018-08-15/heres-a-slug/', content
