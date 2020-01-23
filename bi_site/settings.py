c0821f8224dce7af7902252e7dea9f619bf19220
Chad DeRosier <chad.derosier@gmail.com>
2020-01-23 00:59:43 -0800

    updated settings.py

--- bi_site/settings.py
+++ bi_site/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['157.245.247.75', 'brilliantimagery.org', 'www.brilliantimagery.org']
 
 
 TINYMCE_DEFAULT_CONFIG = {
@@ -162,7 +162,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
